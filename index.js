c08d869f9304d54cc3f893c02c49ae9ab0a726b7
magistrigusti <magisrigusti@gmail.com>
2024-11-04 14:15:47 +0200

    0.1a

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import Web3 from 'web3';
 
+const exchangeAddress = factoryContract.methods.getExchange(tokenAddress);
+const factoryContract = new web3.eth.Contract(factoryABI, factoryAddress);
+const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
 
 const web3 = new Web3("https://ethereum-sepolia-rpc.publicnode.com");
 const privateKey = process.env.PRIVATE_KEY;
@@ -10,5 +13,7 @@ const account = web3.eth.accounts.wallet.add(privateKey);
 //   .then(data => console.log(data));
 // web3.eth.getChainId().then(data => console.log(data));
 // web3.eth.getBlockNumber().then(data => console.log(data));
-web3.eth.getTransaction("0xa749ad807bd110333929a51791cbc9708eb18d4fe7fca0892f7ee436e6a75f49")
-  .then(data => console.log(data));
+// web3.eth.getTransaction("0xa749ad807bd110333929a51791cbc9708eb18d4fe7fca0892f7ee436e6a75f49")
+//   .then(data => console.log(data));
+
+const uniAddress = "0x8efE9DB29DB3826BE04147D8cf01Dc9F7917dCeE";
