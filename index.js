2cc781023b14db54047d5fba3f040b57d80edacc
magistrigusti <magisrigusti@gmail.com>
2024-11-04 11:13:08 +0200

    0.1

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import 'dotenv/config';
-import { Web3 } from 'web3';
+import Web3 from 'web3';
+
 
 const web3 = new Web3("https://ethereum-sepolia-rpc.publicnode.com");
 const privateKey = process.env.PRIVATE_KEY;
 const account = web3.eth.accounts.wallet.add(privateKey);
 
-web3.eth.getBalance(account[0].address)
-  .then(data => console.log(data))
+// web3.eth.getBalance(account[0].address)
+//   .then(data => console.log(data));
+// web3.eth.getChainId().then(data => console.log(data));
+// web3.eth.getBlockNumber().then(data => console.log(data));
+web3.eth.getTransaction("0xa749ad807bd110333929a51791cbc9708eb18d4fe7fca0892f7ee436e6a75f49")
+  .then(data => console.log(data));
